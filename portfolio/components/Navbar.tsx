5ab6f1340c00e9b0c9e3d0a2f509ef57b5d5ce3a
bobromero <rromero123102@gmail.com>
2025-01-03 12:38:18 -0500

    fixed up navbar to be responsive

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -5,27 +5,29 @@ type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div className='navbar'>
-
-
-      <div className='center'>
-        <div className='center navbar-name'>
-          <h1 className='font-bold font-sans'>Robert Romero</h1>
-        </div>
-          
+    <div>
+      <div className='center navbar-name'>
+        <h1 className='font-bold font-sans'>Robert Romero</h1>
       </div>
+      <nav className='navbar'>
+          <div className='middle flex justify-end'>
+            <Link className='navbar-text' href="/"><h3 className='p-1'>SUMMARY</h3></Link>
+          </div>
 
-      <div className=''>
-        <Link className='navbar-text flex justify-self-end' href="/"><h3 className='font-sans font-medium'>SUMMARY</h3></Link>
-      </div>
+          <div className='center '>
+            <Link className='navbar-text' href="/projects"><h3 className='p-1'>PROJECTS</h3></Link>
+          </div>
 
-      <div className='center'>
-        <Link className='navbar-text center' href="/projects"><h3 className='font-sans font-medium'>PROJECTS</h3></Link>
-      </div>
+          <div className='middle'>
+            <Link className='navbar-text' href="/contact"><h3 className='p-1'>CONTACT INFO</h3></Link>
+          </div>
 
-      <div className=''>
-        <Link className='navbar-text flex justify-self-start' href="/contact"><h3 className='font-sans font-medium '>CONTACT INFO</h3></Link>
-      </div>
+      </nav>
+
+      
+
+      
+
 
     </div>
   )
