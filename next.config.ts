c92db3b93e46a964de0dbd72f14ef47000af3089
bobromero <rromero123102@gmail.com>
2025-01-06 12:41:26 -0500

    update

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Portfolio",
   output:"export",
-  eslint: {
-    ignoreDuringBuilds: true,
-    
-},
 };
 
 export default nextConfig;
