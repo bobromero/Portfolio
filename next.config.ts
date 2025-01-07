37fb22bad6ff386bccd164139579b2ef4edf9d2c
bobromero <rromero123102@gmail.com>
2025-01-07 11:07:28 -0500

    Revert "update next config"
    This reverts commit c862dcb0f2b584727f7822d175a449b9b5013d3b.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Portfolio",
   output:"export",
   eslint: {
     ignoreDuringBuilds: true,
