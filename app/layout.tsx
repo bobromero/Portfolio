88b395ca0b41fb82dd2e2eb15342e56a963369ef
bobromero <rromero123102@gmail.com>
2025-03-24 13:08:05 -0400

    new line change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,18 @@ import "./globals.css"
 import Contact from "@/components/Contact"
 
 export default function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <body>
-          <Navbar></Navbar>
-          <main>{children}</main>
-          <Contact></Contact>
-        </body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <body>
+        <Navbar></Navbar>
+        <main>{children}</main>
+        <Contact></Contact>
+      </body>
+    </html>
+  )
+}
