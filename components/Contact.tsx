7de07a28dbb9f938ebe9cdb3f51dbeaf82e3ea0e
bobromero <rromero123102@gmail.com>
2025-03-24 13:08:36 -0400

    changing contact images

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 function Contact() {
   return (
     <div className='cotact-background'>
-      <div  id='contact-grid'>
+      <div id='contact-grid'>
 
-        <div >
-            <a className='contact' href="https://github.com/bobromero" target="_blank" rel="noopener noreferrer">
-              <img className='contact-image' src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' alt='Github'></img>
-            </a>
+        <div>
+          <a href="https://github.com/bobromero" target="_blank" rel="noopener noreferrer">
+            <img className='contact-image' src="/Images/github.png" alt='Github'></img>
+          </a>
         </div>
-        <div className=''>
-            <a className='contact' href="https://www.linkedin.com/in/robert-romero-a61403291/" target="_blank" rel="noopener noreferrer">
-              <img className='contact-image' src='https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png' alt='LinkedIn'></img>
-            </a>
+        <div>
+          <a href="https://www.linkedin.com/in/robert-romero-a61403291/" target="_blank" rel="noopener noreferrer">
+            <img className='contact-image' src="/Images/linkedIn.png" alt='LinkedIn'></img>
+          </a>
         </div>
-        <div className='contact middle'>
-            <p className=''>ROBROMERO@GMAIL.COM</p>
+        <div className='middle'>
+          <p className=''>ROBROMERO@GMAIL.COM</p>
         </div>
 
       </div>
