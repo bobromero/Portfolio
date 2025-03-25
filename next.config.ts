d7a9e9d647065db2ce30dbb366b38fa7fe907a83
bobromero <rromero123102@gmail.com>
2025-03-25 12:17:11 -0400

    added flag for images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/Portfolio",
   output: "export",
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  }
 };
 
 export default nextConfig;
