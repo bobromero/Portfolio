3f9c905a944d958401342172676580a31411a42a
bobromero <rromero123102@gmail.com>
2025-03-25 10:41:43 -0400

    changed github workflow settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Portfolio",
+  basePath: process.env.PAGES_BASE_PATH,
   output: "export",
 };
 
