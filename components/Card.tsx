cf72af804553e20f58005f3e4729752fe1ef9d20
bobromero <rromero123102@gmail.com>
2025-05-24 14:50:29 -0400

    fixed import

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import CardInfo from './info/CardInfo'
+import { CardInfo } from './info/CardInfo'
 
 function Card({ Title, description, Image, Link }: CardInfo) {
-  return (
-    <div className='card'>
-      <a className='card-grid' href={Link} target="_blank" rel="noopener noreferrer">
-        <h1 className='card-title'>
-          {Title}
-        </h1>
-        <div className='center'>
-          <img className='card-image' src={Image} alt={Title + ' image'}></img>
-        </div>
+    return (
+        <div className='card'>
+            <a className='card-grid' href={Link} target="_blank" rel="noopener noreferrer">
+                <h1 className='card-title'>
+                    {Title}
+                </h1>
+                <div className='center'>
+                    <img className='card-image' src={Image} alt={Title + ' image'}></img>
+                </div>
 
-        <p className='card-description main-page-font'>
-          {description}
-        </p>
-      </a>
-    </div>
-  )
+                <p className='card-description main-page-font'>
+                    {description}
+                </p>
+            </a>
+        </div>
+    )
 }
 
 export default Card
