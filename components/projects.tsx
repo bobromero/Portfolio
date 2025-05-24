e5dcfa1b1ddd580d8cd316b29b3d59b2dd0cd5ca
bobromero <rromero123102@gmail.com>
2025-05-24 14:52:11 -0400

    fixed import

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-import CardInfo from './info/CardInfo'
+import { CardInfo } from './info/CardInfo'
 
 type Props = {}
 
-function projects({}: Props) {
+function projects({ }: Props) {
 
-//create a function to create cards and project card based on json
+    //create a function to create cards and project card based on json
 
-//the function looks at the cards folder and returns an array of htmx elements
-
-
-  return (
-    <div>
-      projects
+    //the function looks at the cards folder and returns an array of htmx elements
 
 
-    </div>
-  )
+    return (
+        <div>
+            projects
+
+
+        </div>
+    )
 }
 
 export default projects
