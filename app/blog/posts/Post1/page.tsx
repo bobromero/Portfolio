5524329b3ac77495f92954d618542a119a0611ca
bobromero <rromero123102@gmail.com>
2025-05-24 16:30:21 -0400

    first blog post

--- app/blog/posts/Post1/page.tsx
+++ app/blog/posts/Post1/page.tsx
@@ -5,7 +5,29 @@ function Post1() {
     return (
         <div className="post">
             <h1>First blog post!</h1>
-            <p>This is my first blog post!</p>
+            <div className="post-content">
+                <div className="flex center"><img className="postimg" src="/Portfolio/Images/Bobert.png" alt="w mans"></img></div>
+                <p>
+                    Hello, my name is Robert and I like philosophy, golf and computers.
+                    On my free time, you can find me at the local golf courses, or studying in the library(Proud Diracian). I am a pretty normal College student, but I do some other more unique stuff too haha.
+                    <br></br>
+                    <br></br>
+                    I keep the ghosts of tallahassee contained as seen in this picture.
+                    I am the ghostbuster of tally and have irrefutable proof of such.
+                    Most people think ghost aren't real, but I assure you that they are, and that they are watching you RIGHT NOW!
+                    Don't worry thogh, they are mostly harmless.
+                    If you encounter a ghost acting up, make sure to not listen to what it says, and give me a call.
+                    <br></br>
+                    <br></br>
+                    Golfing is my favorite pass time, and even though I am not the greatest I still enjoy the process of getting better.
+                    Patience is a virtue and sometimes I don't have enough of it, but who does!
+                    <br></br>
+                    <br></br>
+                    But yeah, thats it for me. Signing off, and always remember, the ghost are watching. :)
+
+
+                </p>
+            </div>
 
 
         </div>
