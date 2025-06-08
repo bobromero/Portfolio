0f3a3e2cb82d978fd29e22ae8bdba600eb018bb5
bobromero <rromero123102@gmail.com>
2025-06-08 11:57:39 -0400

    post3

---
+++ app/blog/posts/Post3/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../../blog.css"
+
+
+function Post1() {
+    return (
+        <div className="post">
+            <h1>Second Blog Post, MIT App Inventor!</h1>
+            <div className="post-content">
+                <div className="flex center">
+                    <img className="postimg" src="/Portfolio/Images/BlogPost2/Selfie.png" alt="Selfie"></img>
+                </div>
+                <p>
+                    My group and I are making a clicker game for our project. We met via facetime and decided on a retake on a classic. You shake your phone and a ball bounces around the screen. The more it bounces, the more points you get and the more you can progress.
+
+                    <br></br>
+                    <br></br>
+
+                    We have BIG IDEAS for this project and have high expectations for it. My group members are great and I look forward to working with them.
+
+                    <br></br>
+                    <br></br>
+
+
+                    We already have a working prototype with a ball that bounces and speeds up when shaking! Very exciting!
+                </p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Post1
