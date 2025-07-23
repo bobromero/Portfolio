d20c34bf052851b903fc0cb5b3365ab0ef58d3a4
bobromero <rromero123102@gmail.com>
2025-07-23 10:19:21 -0400

    added post 6

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,33 +1,34 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import "./blog.css"
+import "./blog.css";
 
 function blog() {
-    return (
-        <div id="posts" >
+  return (
+    <div id="posts">
+      <h1>Blog Posts</h1>
 
-            <h1>Blog Posts</h1>
-
-            <div>
-                <Link href="/blog/posts/Post1">
-                    <h3>My First Blog Post</h3>
-                </Link>
-                <Link href="/blog/posts/Post2">
-                    <h3>My Second Blog Post</h3>
-                </Link>
-                <Link href="/blog/posts/Post3">
-                    <h3>Group Project Started!</h3>
-                </Link>
-                <Link href="/blog/posts/Post4">
-                    <h3>Tinker Cad Objects</h3>
-                </Link>
-                <Link href="/blog/posts/Post5">
-                    <h3>Free Cad Objects</h3>
-                </Link>
-            </div>
-
-        </div>
-    )
+      <div>
+        <Link href="/blog/posts/Post1">
+          <h3>My First Blog Post</h3>
+        </Link>
+        <Link href="/blog/posts/Post2">
+          <h3>My Second Blog Post</h3>
+        </Link>
+        <Link href="/blog/posts/Post3">
+          <h3>Group Project Started!</h3>
+        </Link>
+        <Link href="/blog/posts/Post4">
+          <h3>Tinker Cad Objects</h3>
+        </Link>
+        <Link href="/blog/posts/Post5">
+          <h3>Free Cad Objects</h3>
+        </Link>
+        <Link href="/blog/posts/Post6">
+          <h3>Golf Tee Holder</h3>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default blog 
+export default blog;
---
+++ app/blog/posts/Post6/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../blog.css";
+
+function Post6() {
+  return (
+    <div className="post">
+      <h1>Fabricating for a Function</h1>
+      <div className="post-content">
+        <div className="flex center cad-post">
+          <p className="post-text">
+            I already have a holder for my golf balls on my golf bad, but it
+            would be really nice to have an accessible holder for golf tees.
+          </p>
+          <a href="https://www.youtube.com/shorts/ErLtiQFURug">Link to video</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post6;
